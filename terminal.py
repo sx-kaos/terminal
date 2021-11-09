27fa2a75f808a75cca6c9120874ecd81c3fdf6b3
kaos <79645506+sx-kaos@users.noreply.github.com>
2021-11-09 22:28:32 +0000

    re made the entire thing, now works much better
     will be pushing updates frequently (hopefully)

--- terminal.py
+++ terminal.py
@@ -2,9 +2,11 @@ import os
 import time
 import pynput
 import datetime
+import time
 import psutil
 import platform
 import webbrowser
+from datetime import *
 import random
 import GPUtil
 import cpuinfo
@@ -12,240 +14,138 @@ import string
 import colorama
 import ctypes
 import sys
+from time import sleep
 from os import * 
 from colorama import init
 from win32api import GetSystemMetrics
-from pynput.keyboard import Key, Listener
-from pynput import *
+from alive_progress import alive_bar
+from halo import Halo
 
-init()
+#gathering all system information needed
+class needed():
+    ctypes.windll.kernel32.SetConsoleTitleW(f" ")
+    spinner = Halo(text='Loading', spinner='dots', color = 'cyan') #front end
+    spinner.start() #loading screen whilst gathering system info
+    today = date.today()
+    now = datetime.now()
+    date = today.strftime("%B %d, %Y")
+    username = os.getlogin()
+    os_name = platform.system()
+    os_release = platform.release()
+    cpu = cpuinfo.get_cpu_info()['brand_raw']
+    monitor_width = GetSystemMetrics(0)
+    monitor_height = GetSystemMetrics(1)
+    lib = ctypes.windll.kernel32
+    t = lib.GetTickCount64()
+    t = int(str(t)[:-3])
+    mins, sec = divmod(t, 60)
+    hour, mins = divmod(mins, 60)
+    days, hour = divmod(hour, 24)
+    ram = round(psutil.virtual_memory().total/1000000000, 2) #in Gigabytes
+    spinner.stop() # stop loading screen once system info gathered
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+#colors using ansi escape sequences
 class c:
 	grey = '\033[1;30;40m'
 	red = '\033[1;31;40m'
 	dark_red = '\033[2;31;40m'
-	green  = '\033[1;32;40m'
-	yellow  = '\033[1;33;40m'
+	green = '\033[1;32;40m'
+	yellow = '\033[0;33;40m'
 	blue = '\033[1;34;40m'
 	magenta = '\033[1;35;40m'
 	cyan = '\033[1;36;40m'
 	white = '\033[1;37;40m'
+	black = '\033[1;30;40m'
 
-
+#terminal style typing
 def print_slow(str):
     for letter in str:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(0.275)
+        time.sleep((random.randrange(0.001, 0.275)))
 
+print(f"""
+{c.cyan}╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+{c.cyan}│    {c.white}__                                                                      {c.cyan}│
+{c.cyan}│   {c.white}/ /  __  __   ____   ____ _   _____                                      {c.cyan}│
+{c.cyan}│  {c.white}/ /  / / / /  / __ \ / __ `/  / ___/      Welcome to lunar                {c.cyan}│
+{c.cyan}│ {c.white}/ /  / /_/ /  / / / // /_/ /  / /          Type help for a list of cmds    {c.cyan}│
+{c.cyan}│{c.white}/_/   \____/  /_/ /_/ \____/  /_/                                           {c.cyan}│
+{c.cyan}│                                                                            {c.cyan}│
+{c.cyan}╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+""")
 def main():
-	def hack():
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("""\033[1;32;40m
-			┌─┐┌┐┌┌┬┐┌─┐┬─┐┬┌┐┌┌─┐  ┬ ┬┌─┐┌─┐┬┌─┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐
-			├┤ │││ │ ├┤ ├┬┘│││││ ┬  ├─┤├─┤│  ├┴┐├┤ ├┬┘  ││││ │ ││├┤ 
-			└─┘┘└┘ ┴ └─┘┴└─┴┘└┘└─┘  ┴ ┴┴ ┴└─┘┴ ┴└─┘┴└─  ┴ ┴└─┘─┴┘└─┘o
-	""")
-		time.sleep(0.35)
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("""\033[1;32;40m
-			┌─┐┌┐┌┌┬┐┌─┐┬─┐┬┌┐┌┌─┐  ┬ ┬┌─┐┌─┐┬┌─┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐
-			├┤ │││ │ ├┤ ├┬┘│││││ ┬  ├─┤├─┤│  ├┴┐├┤ ├┬┘  ││││ │ ││├┤ 
-			└─┘┘└┘ ┴ └─┘┴└─┴┘└┘└─┘  ┴ ┴┴ ┴└─┘┴ ┴└─┘┴└─  ┴ ┴└─┘─┴┘└─┘oo
-	""")
-		time.sleep(0.35)
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("""\033[1;32;40m
-			┌─┐┌┐┌┌┬┐┌─┐┬─┐┬┌┐┌┌─┐  ┬ ┬┌─┐┌─┐┬┌─┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐
-			├┤ │││ │ ├┤ ├┬┘│││││ ┬  ├─┤├─┤│  ├┴┐├┤ ├┬┘  ││││ │ ││├┤ 
-			└─┘┘└┘ ┴ └─┘┴└─┴┘└┘└─┘  ┴ ┴┴ ┴└─┘┴ ┴└─┘┴└─  ┴ ┴└─┘─┴┘└─┘ooo
-	""")
-		time.sleep(0.35)
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("""\033[1;32;40m
-                                ┬ ┬┌─┐┌─┐┬┌─┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐
-                                ├─┤├─┤│  ├┴┐├┤ ├┬┘  ││││ │ ││├┤
-                                ┴ ┴┴ ┴└─┘┴ ┴└─┘┴└─  ┴ ┴└─┘─┴┘└─┘
-                                """)
-		print("")
-		victim = input("person to hack: ")
-		print(f"hacking {victim}...")
-		time.sleep(1.5)
-		print(f"Gathering {victim}'s data")
-		time.sleep(1.5)
-		print(f"Stealing all {victim}'s money")
-		time.sleep(1.5)
-		print("")
-		print(f"Successfully hacked {victim}")
-		print("")
-
-
-	a = os.getlogin()
-	os_name = platform.system()
-	os_release = platform.release()
-	grey = '\033[1;30;40m'
-	red = '\033[0;31;40m'
-	command = input(f"{a}@{os_name}:~$ ")
-	valid_commands = ['cls',
-                                'cd',
-                                'clear',
-                                'neofetch',
-                                'dir',
-                                'help',
-                                'time',
-                                'date',	
-                                '',
-                                ' ',
-                                'info',
-                                'report',
-                                'search',
-                                'ping',
-                                'exit']
+	print(f"{c.white}╭{needed.username}@lunar")
+	command = input(f"{c.white}╰─→ ")
+	valid = ['neofetch',
+			 'help',
+			 'cls',
+			 'clear',
+			 'date',
+			 'time',
+			 'google',
+			 'ping',
+			 '',
+			 '	']
 	if command == 'cls':
 		os.system('cls' if os.name == 'nt' else 'clear')
-		main()
-	elif command == 'clear':
-		os.system('cls' if os.name == 'nt' else 'clear')
-		main()
-	elif command == 'neofetch':
-		cpu = cpuinfo.get_cpu_info()['brand_raw']
-		width = GetSystemMetrics(0)
-		height = GetSystemMetrics(1)
-		if os_name == 'Windows' or 'windows':
-			lib = ctypes.windll.kernel32
-			t = lib.GetTickCount64()
-			t = int(str(t)[:-3])
-			mins, sec = divmod(t, 60)
-			hour, mins = divmod(mins, 60)
-			days, hour = divmod(hour, 24)
-			print(f"""
-
-	                               \033[1;36;40m..,    \033[1;36;40mjackk\033[1;37;40m@\033[1;36;40m{os_name}
-	\033[1;36;40m		    ....,,:;+ccllll
-	\033[1;36;40m      ...,,+:;  cllllllllllllllllll   OS: \033[1;37;40m{os_name} {os_release}
-	\033[1;36;40m,cclllllllllll  lllllllllllllllllll   Host: \033[1;37;40mMWIP
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Kernel: \033[1;37;40mWIP
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Uptime: \033[1;37;40m{days} day(s), {hour:02} hours, {mins:02} mins
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Packages: \033[1;37;40mWIP
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Shell: \033[1;37;40mSmiley terminal 2021
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Resolution: \033[1;37;40m{width}x{height}
-										
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   Terminal: \033[1;37;40mSmiley
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   WM: \033[1;37;40mWIP
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   WM Theme: \033[1;37;40mWIP
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   CPU: \033[1;37;40m{cpu}
-	\033[1;36;40mllllllllllllll  lllllllllllllllllll   GPU: \033[1;37;40m WIP
-	\033[1;36;40m`'ccllllllllll  lllllllllllllllllll   Memory: \033[1;37;40m{round(psutil.virtual_memory().total/1000000000, 2)} GB
-	\033[1;36;40m       `' \*::  :ccllllllllllllllll
-	\033[1;36;40m                       ````''*::cll
-	\033[1;36;40m                               ``
-
-
-		""")
-		main()
-	elif command == 'tree':
-		system('tree')
-		main()
-	elif command == 'dir':
-		system('dir')
-		main()
-	elif command == 'help':
-		print("""Commands:
-\033[1;36;40mcls - \033[1;37;40mClears screen
-\033[1;36;40mcd - \033[1;37;40mShows current directory
-\033[1;36;40mclear - \033[1;37;40mSame as cls
-\033[1;36;40mneofetch - \033[1;37;40mDisplays system info
-\033[1;36;40mdir - \033[1;37;40mShows all files in current directory 
-\033[1;36;40mhelp - \033[1;37;40mTakes you here 
-\033[1;36;40minfo - \033[1;37;40mGives a little more info about this project
-\033[1;36;40mreport - \033[1;37;40mReport any bugs
-\033[1;36;40msearch - \033[1;37;40mOpen a url onto a new tab
-\033[1;36;40mping - \033[1;37;40mPings specified IP/website/server
-\033[1;36;40mrandstr - \033[1;37;40moutputs a random string (combination of letters and numbers)
-\033[1;36;40mrandint - \033[1;37;40moutputs a random string of numbers (numbers only)
-\033[1;36;40mexit - \033[1;37;40mExits the terminal (pressing ctrl + c does the same thing)
-	""")
-		print("Just a quick heads up, the commands are case sensitive, e.g. if you type neofetch with a space at the end it wont work")
-		print("")
-		main()
-	elif command =='cd':
-		os.system('cd')
-		main()
-	elif command == '?':
-		print("""Commands:
-\033[1;36;40mcls - \033[1;37;40mClears screen
-\033[1;36;40mcd - \033[1;37;40mShows current directory
-\033[1;36;40mclear - \033[1;37;40mSame as cls
-\033[1;36;40mneofetch - \033[1;37;40mDisplays system info
-\033[1;36;40mdir - \033[1;37;40mShows all files in current directory 
-\033[1;36;40mhelp - \033[1;37;40mTakes you here 
-\033[1;36;40minfo - \033[1;37;40mGives a little more info about this project
-\033[1;36;40mreport - \033[1;37;40mReport any bugs
-\033[1;36;40msearch - \033[1;37;40mOpen a url onto a new tab
-\033[1;36;40mping - \033[1;37;40mPings specified IP/website/server
-\033[1;36;40mrandstr - \033[1;37;40moutputs a random string (combination of letters and numbers)
-\033[1;36;40mrandint - \033[1;37;40moutputs a random string of numbers (numbers only)
-\033[1;36;40mexit - \033[1;37;40mExits the terminal (pressing ctrl + c does the same thing)
-	""")
-		print("Just a quick heads up, the commands are case sensitive, e.g. if you type neofetch with a space at the end it wont work")
-		print("")
-		main()
-	elif command == 'info':
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("You are currently using smiley terminal")
-		print("It is alternative to your OS's terminal")
-		print("It is still in development so please report any bugs by typing 'report'")
-		print("All commands can be found by typing 'help'")
-		main()
-	elif command == 'report':
-		print("you can report any bugs on our discord server")
-		print("https://discord.gg/NSU4pUnEQS")
-		main()
-	elif command == 'search':
-		site = input("link: ")
-		webbrowser.open_new_tab(site)
-		main()
-	elif command == 'ping':
-		ip = input("Target: ")
-		system(f'ping {ip}')
-		main()
-	elif command ==	'randint':
-		print(random.randint(0, 1000))
-		main()
-	elif command == 'randstr':
-		print("".join(random.choices(string.ascii_lowercase + string.digits, k = 10)))
-		main()
-	elif command == 'banner':
-		print("""
-                    _   _                
-                   (_) | |               
-  ___   _ __ ___    _  | |   ___   _   _ 
- / __| | '_ ` _ \  | | | |  / _ \ | | | |
- \__ \ | | | | | | | | | | |  __/ | |_| |
- |___/ |_| |_| |_| |_| |_|  \___|  \__, |
-                                    __/ |
-                                   |___/ 
-
+		print(f"""
+{c.cyan}╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+{c.cyan}│    {c.white}__                                                                      {c.cyan}│
+{c.cyan}│   {c.white}/ /  __  __   ____   ____ _   _____                                      {c.cyan}│
+{c.cyan}│  {c.white}/ /  / / / /  / __ \ / __ `/  / ___/      Welcome to lunar                {c.cyan}│
+{c.cyan}│ {c.white}/ /  / /_/ /  / / / // /_/ /  / /          Type help for a list of cmds    {c.cyan}│
+{c.cyan}│{c.white}/_/   \____/  /_/ /_/ \____/  /_/                                           {c.cyan}│
+{c.cyan}│                                                                            {c.cyan}│
+{c.cyan}╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
 """)
-		main()
-	elif command ==	'reload':
-		system('start terminal.py')
-		exit()
-	elif command == 'hack':
-		hack()
 		main()
 	elif command == '':
 		main()
-	elif command == ' ':
+	elif command == 'clear':
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print(f"""
+{c.cyan}╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+{c.cyan}│    {c.white}__                                                                      {c.cyan}│
+{c.cyan}│   {c.white}/ /  __  __   ____   ____ _   _____                                      {c.cyan}│
+{c.cyan}│  {c.white}/ /  / / / /  / __ \ / __ `/  / ___/      Welcome to lunar                {c.cyan}│
+{c.cyan}│ {c.white}/ /  / /_/ /  / / / // /_/ /  / /          Type help for a list of cmds    {c.cyan}│
+{c.cyan}│{c.white}/_/   \____/  /_/ /_/ \____/  /_/                                           {c.cyan}│
+{c.cyan}│                                                                            {c.cyan}│
+{c.cyan}╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+""")
 		main()
-	if ' ' in command:
-		pass
-	if '	' in command:
+	elif command == 'neofetch':
+		print(f"╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮")
+		print(f" {c.cyan}OS:{c.white} {needed.os_name} {needed.os_release}")
+		print(f" {c.cyan}Uptime:{c.white} {needed.days} Day(s), {needed.hour:02} Hour(s), {needed.mins:02} Min(s)")
+		print(f" {c.cyan}Resolution:{c.white} {needed.monitor_width}x{needed.monitor_height}")
+		print(f" {c.cyan}CPU:{c.white} {needed.cpu}")
+		print(f" {c.cyan}RAM:{c.white} {needed.ram}")
+		print(f"╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯")
 		main()
-	elif command not in valid_commands:
-		print(f"\033[1;32;40m{command} is not recognized as an internal or external command {c.white}")
+	elif command == 'help':
+		print(f"commands:\nhelp - shows all commands\ncls/clear - clears terminal screen\ndate/time - shows the date/time\ngoogle - googles the argument given, e.g. google youtube\nping - pings a domain/IP address\nneofetch - displays (some) system information")
 		main()
-
-
-		
+	elif command == 'date':
+		print(f"{needed.date}")
+		main()
+	elif command == 'time':
+		now = datetime.now()
+		time2 = now.strftime("%H:%M:%S")
+		print(f"{time2}")
+		main()
+	elif command == 'google':
+		query = input("What do you want to search up? ")
+		webbrowser.open_new_tab(query)
+		main()
+	elif command == 'ping':
+		target = input("Target to ping: ")
+		os.system(f'ping {target}')
+		command()
+	elif command not in valid:
+	    print(f"{command} is not recognised as a valid command")
+	    main()
 main()
+
